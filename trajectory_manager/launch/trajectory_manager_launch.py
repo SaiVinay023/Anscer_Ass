00bb6a5ab729876d86add810d5a4aa3e7f3e13a5
SaiVinay023 <saivinay023@gmail.com>
2025-03-08 07:35:51 +0530

    trying to launch gazebo and launch file seperately

--- trajectory_manager/launch/trajectory_manager_launch.py
+++ trajectory_manager/launch/trajectory_manager_launch.py
@@ -1,29 +1,39 @@
 import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import TimerAction
+from launch.actions import TimerAction, DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 def generate_launch_description():
     rviz_config_path = os.path.join(
         os.path.expanduser("~"), "ros2_ws", "src", "Anscer_Ass", "trajectory_manager", "rviz", "trajectory_manager.rviz"
     )
 
-    spawn_robot = TimerAction(
-        period=5.0,  # Wait for Gazebo to fully start
-        actions=[
-            Node(
-                package="gazebo_ros",
-                executable="spawn_entity.py",
-                arguments=[
-                    "-entity", "burger",
-                    "-file", "/opt/ros/humble/share/turtlebot3_gazebo/models/turtlebot3_burger/model.sdf"
-                ],
-                output="screen"
-            )
-        ]
-    )
+   # gazebo = Node(
+   # executable="/usr/bin/gzserver",
+   # arguments=["--verbose", "-s", "libgazebo_ros_init.so", "-s", "libgazebo_ros_factory.so"],
+   # output="screen"
+#)
+
+   # spawn_robot = TimerAction(
+    #    period=5.0,  # Wait for Gazebo to fully start before spawning the robot
+    #    actions=[
+     #       Node(
+      #          package="gazebo_ros",
+      #          executable="spawn_entity.py",
+      #          arguments=[
+      #              "-entity", "burger",
+       #             "-file", "/opt/ros/humble/share/turtlebot3_gazebo/models/turtlebot3_burger/model.sdf"
+       #         ],
+       #         output="screen"
+        #    )
+       # ]
+   # )
 
     return LaunchDescription([
+        #gazebo,  # Start Gazebo first
+  # Start the Gazebo GUI
+        #spawn_robot,  # Wait and then spawn the robot
         Node(
             package="trajectory_manager",
             executable="trajectory_publisher_saver",
@@ -42,11 +52,5 @@ def generate_launch_description():
             name="rviz2",
             output="screen",
             arguments=["-d", rviz_config_path]
-        ),
-        Node(
-    package="gazebo_ros",
-    executable="gzserver",
-    arguments=["-s", "libgazebo_ros_init.so", "-s", "libgazebo_ros_factory.so", "~/ros2_ws/src/Anscer_Ass/trajectory_manager/worlds/turtlebot3_world.world"],
-    output="screen"
-)  # Ensure this is inside the list
+        )
     ])
